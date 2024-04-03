faef6ab3a665557e78b75ddf3afac83ecc0fcccc
fenglongyu <fenglongyu@163.com>
2024-04-03 15:02:27 +0800

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
- 
+
 setup(
     name='arxiv_search_assistant',
     version='0.1',
@@ -8,12 +8,12 @@ setup(
     python_requires='>=3.9',
     install_requires=[
         # list of any packages that need to be installed along with your package
-        feedparser,
-        arxiv,
-        tqdm,
-        openai
+        "feedparser",
+        "arxiv",
+        "tqdm",
+        "openai",
     ],
     author="fenglongyu",
-    Email="fly_fenglongyu@outlook.com"
+    Email="fly_fenglongyu@outlook.com",
     url="https://github.com/flyingwaters/paper_search_assistant.git"
 )
